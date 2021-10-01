edb94b62f1c44388b8793e61a1274408c81c352d
aditya1746 <68777899+aditya1746@users.noreply.github.com>
2021-10-01 21:57:21 +0530

    Update depth_finder.ino

--- depth_finder.ino
+++ depth_finder.ino
@@ -1,21 +1,57 @@
 #include <ros.h>
-#include <std_msgs/String.h>
+#include <ros/time.h>
+#include <sensor_msgs/Range.h>
+#include <std_msgs/Int32.h>
 #include <std_msgs/Float32.h>
+#include <Servo.h>
 
-ros::NodeHandle pressure_sensor;
+#include <Wire.h>
+#include "MS5837.h"
 
+MS5837 sensor;
 
+ros::NodeHandle pressure_input;
 
-ros::Publisher pub('depth', &depth);
+std_msgs::Float32 depth_msg;
 
-void setup()
-{
-    downMotion.initNode();
-    downMotion.advertise(pub);
-    downMotion.subscribe(sub);
+ros::Publisher pub('depth', &depth_msg)
+
+float d;
+
+void setup() {
+
+    pressure_input.initNode();
+
+    pressure_input.advertise(pub);
+
+    Serial.begin(9600);
+    
+    delay(7000); // delay to allow the ESC to recognize the stopped signal<br />
+
+    Wire.begin();
+    
+    while (!sensor.init()) 
+    {
+        Serial.println("Init failed!");
+        delay(1000);
+    }
+
+    sensor.setModel(MS5837::MS5837_30BA);
+    sensor.setFluidDensity(997); 
 }
+ 
+void loop() {
 
-void loop()
-{
+    sensor.read();
+    
+    d = 23 + (sensor.depth()*100);
 
+    depth_msg.data = depth;
+
+    pub.publish(&depth_msg);
+
+    Serial.println(depth); 
+
+    nh.spinOnce();
 }
+            
