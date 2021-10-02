6e060c92fa3db03f148591753fdaeac303d0d33d
aditya1746 <68777899+aditya1746@users.noreply.github.com>
2021-10-02 21:13:08 +0530

    made necessary changes
    After the code was added by Image Processing team

--- distance_finder.py
+++ distance_finder.py
@@ -9,6 +9,12 @@ from cv_bridge import CvBridge
 from std_msgs.msg import String
 from sauvc_qualification.msg import distance_and_center
 
+def nothing(x):
+	pass
+
+def findDistanceFromArea(ar):
+	# will be define once we have actual hardware
+
 # ============= defining callback function for reading the image published on camera_input topic ====================
 
 def callback(msgs):
@@ -17,22 +23,23 @@ def callback(msgs):
 
     bridge = CvBridge()
     img = bridge.imgmsg_to_cv2(msgs, desired_encoding="passthrough")
+	
+	frame_cx,frame_cy,ch = img.shpae
+	
+	frame_cx = frame_cx/2
+	frame_cy = frame_cy/2
 
     #cv2.imshow('frame_sub', img)
     #cv2.waitKey(5000)
     #cv2.destroyAllWindows()
 
     # =============== processing the image  ===================================
-def nothing(x):
-	pass
 
-#image=cv2.imread("test.jpeg")
-image = img
-b, g, r = cv2.split(image)
+    #image=cv2.imread("test.jpeg")
+    image = img
+	b, g, r = cv2.split(image)
 
-
-
-while(1):
+#while(1):
 
 	b1 = b-135
 	g1 = g-144
@@ -46,6 +53,8 @@ while(1):
 	temp = cv2.dilate(edged, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3)))
 
 	cnts,_ = cv2.findContours(temp,cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+	
+	area = -1
 
 	if(len(cnts)>0):
 
@@ -55,7 +64,11 @@ while(1):
 		rect = cv2.minAreaRect(cnt)
 		box = cv2.boxPoints(rect) # cv2.boxPoints(rect) for OpenCV 3.x
 		box = np.int0(box)
-		print(cv2.contourArea(cnt))
+		area = cv2.contourArea(cnt)
+		print(area)
+		M = cv.moments(cnt)
+		cx = int(M['m10']/M['m00'])
+		cy = int(M['m01']/M['m00'])
 		cv2.drawContours(image,[box],0,(0,0,255),1)
 
 	cv2.imshow("final",edged)
@@ -63,9 +76,9 @@ while(1):
 	if cv2.waitKey(30) & 0xFF == 27:
 		break
 
-cv2.waitKey(0)
+	cv2.waitKey(0)
 
-cv2.destroyAllWindows()
+	cv2.destroyAllWindows()
 
 
     # ============== publishing the necessary data on gate_pos topic ===========
@@ -74,17 +87,9 @@ cv2.destroyAllWindows()
 
     msg_dist = distance_and_center()
 
-    if(depth_reached=="no"):
-        
-        msg_dist.distance = -100
-        msg_dist.centerx = -100
-        msg_dist.centery = -100
-
-    else:
-
-        msg_dist.distance = 20
-        msg_dist.centerx = 30
-        msg_dist.centery = 10
+    msg_distance.distance = findDistanceFromArea(area)
+	msg_distance.centerx = frame_cx - cx
+	msg_distance.centery = framecy - cy
 
     pub.publish(msg_dist)
 
